720ac5e69881fa25a1ef9ca649a8b8f945a1b13f
rabobahago <rabobahago@gmail.com>
2021-03-17 11:36:04 +0100

    google map added

---
+++ script.js
@@ -0,0 +1,7 @@
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+    mapId: '17595e04cea46aae',
+  })
+}
